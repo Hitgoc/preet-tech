9ae4f3f41a67ed9dc3245c018f6a488157ecf410
NickAz1611 <pratapnikhil16112001>
2022-08-20 19:41:42 +0530

    commit

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,13 @@
 import Head from "next/head";
+import HomePage from "../src/Home/Home";
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
         <title>Preet Tech</title>
       </Head>
+      <HomePage />
     </div>
   );
 }
---
+++ src/Home/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Page from "./components/Page";
+
+const HomePage = () => {
+  return (
+    <div>
+      <Page />
+    </div>
+  );
+};
+
+export default HomePage;
